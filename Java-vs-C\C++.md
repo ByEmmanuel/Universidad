07615e0adeba867974070578d0e38f7b17dafa99
ByEmmanuel <polar1183@gmail.com>
2024-09-03 23:55:17 +0000

    Segundo dia aprendiendo c++

---
+++ Java-vs-C\C++.md
@@ -0,0 +1,25 @@
+# Tengo que definir cual va a ser la estructura de este archivo .MarkDown
+
+# Diferencias entre Java y C/C++
+
+## Introducción
+Este documento tiene como objetivo comparar las principales diferencias entre los lenguajes de programación Java y C/C++, enfocándose en la sintaxis, gestión de memoria, paradigma de programación, y otros aspectos clave.
+
+Entre ellos me gustaria añadir elementos que yo mismo voy aprendiendo y que ya he aprendido
+
+## Sintaxis Básica
+### Java
+```java
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
+
+## Sintaxis Basica c++
+```Cpp
+int main(int argc, char const *argv[]){
+    leerArray(array);
+    return 0;
+}
+
---
+++ SegundoTest.cpp
@@ -0,0 +1,56 @@
+/*
+    * Nombre: SegundoTest.cpp
+    * Autor: ByEmmanuel
+    * Fecha: 03-09-2024
+    * Descripcion: Programa que lee un array de 5 elementos y los imprime en pantalla
+*/
+
+/*
+ * Las Bibliotecas que terminan en .h son de C, las que no terminan en .h son de C++
+ * Los include con <> c++ los busca directamente en el directorio raiz del compilador
+ * Los include con "" c++ los busca en el directorio actual (Del proyecto)
+ */
+
+#include <iostream>
+
+/* Arrays En C++ */
+
+// Declaracion de un array. El 5 es el tamaño del array (Es decir, cuantos elementos tiene) y epieza desdee el 0
+const int tamanoDelArray = 5;
+int array[tamanoDelArray];
+int leerArray(int tamanoDelArray[]); // Declaracion de la funcion;
+
+// Funcion principal
+int main(int argc, char const *argv[]){
+    leerArray(array);
+    return 0;
+}
+
+// Definicion de la funcion
+int leerArray(int array[]){
+
+    /* Bucle for que empeieza por 0 y termina en el tamaño del array
+     * std es un espacio de nombres que contiene las funciones de entrada y salida (namespace)
+    * cout es la funcion que imprime en pantalla (output)
+    * cin es la funcion que lee de la entrada estandar (input)
+    * << es el operador de insercion que inserta el valor de la variable en el stream de salida
+    * endl es un caracter de escape que indica el fin de linea
+    * :: es el operador de resolucion de ambito
+    
+    */
+
+    for (int i = 0; i < tamanoDelArray; i++){
+        std::cout << "Ingrese el valor del elemento " << i << ": ";
+        //std::cin >> array[i]; es la forma de leer un valor de la entrada estandar y guardarlo en la variable array[i]
+        // es decir, cin (Ingresa un valor y lo asigno a la variable array[i]) con el operador de insercion <<  || cin >> array[i]
+        std::cin >> array[i];
+    }
+
+
+    for (int i = 0; i < tamanoDelArray; i++){
+        std::cout << "El valor del elemento " << i << " es: " << array[i] << std::endl;
+    }
+    
+
+    return 0;
+};
