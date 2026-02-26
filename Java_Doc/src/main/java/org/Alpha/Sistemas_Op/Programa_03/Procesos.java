cce80ef117ea7cecd6a01058435ca0d0bbb651d4
ByEmmanuel <polar1183@gmail.com>
2026-02-26 10:34:30 -0600

    Add: Programa_03 V0.3 Funcion inestable ejecutarProcesos Sistemas_Op

--- Java_Doc/src/main/java/org/Alpha/Sistemas_Op/Programa_03/Procesos.java
+++ Java_Doc/src/main/java/org/Alpha/Sistemas_Op/Programa_03/Procesos.java
@@ -1,13 +1,14 @@
-package org.Alpha.Sistemas_Op.Programa_03;
+//package org.Alpha.Sistemas_Op.Programa_03;
 
 import java.util.Random;
 
 public class Procesos implements SoInterface{
     private static final Random random = new Random();
     private final int PID = random.nextInt(1001);
-    private final int tiempoMax = random.nextInt(20 - 6 + 1) + 6;
+    private int tiempoMax = random.nextInt(20 - 6 + 1) + 6;
     private final String operacion = obtenerOperador();
     private final int[] operadores = new int[]{random.nextInt(100),random.nextInt(100)};
+    private int tiempoLlegada = 0;
     private String estado = null; // este lo define el sistema
 
 
@@ -46,5 +47,17 @@ public class Procesos implements SoInterface{
 
     public void setEstado(String estado) {
         this.estado = estado;
+    }
+
+    public void setTiempoMax(int tiempoMax) {
+        this.tiempoMax = tiempoMax;
+    }
+
+    public void setTiempoLlegada(int hora){
+        this.tiempoLlegada = hora;
+    }
+
+    public int getTiempoLlegada(){
+        return this.tiempoLlegada;
     }
 }
--- Java_Doc/src/main/java/org/Alpha/Sistemas_Op/Programa_03/Programa_03.java
+++ Java_Doc/src/main/java/org/Alpha/Sistemas_Op/Programa_03/Programa_03.java
@@ -1,4 +1,4 @@
-package org.Alpha.Sistemas_Op.Programa_03;
+//package org.Alpha.Sistemas_Op.Programa_03;
 
 /*
 * Algoritmo de planificacion FCFS
@@ -8,17 +8,36 @@ package org.Alpha.Sistemas_Op.Programa_03;
 
 import java.util.*;
 
+class Tiempo {
+    private long inicio = 0;
+    private long fin = 0;
+
+    public void init() { inicio = System.nanoTime(); }
+    public void end() { fin = System.nanoTime(); }
+    public long calculateTime_milis() {
+        return (long) ((fin - inicio) / 1_000_000.0);
+    }
+}
+
 public class Programa_03 extends AbstractProcesos implements SoInterface{
     private static ArrayList<Procesos> colaProcesos = new ArrayList<>();
+    private static ArrayDeque<Procesos> colaProcesosPrioritario = new ArrayDeque<>();
+    private static final short ticksTiempoMilis = 300;
+    private static int tiempoTranscurridoTicks= 0;
+    private static int indiceParaNuevos = 0;
     int contadorGlobalProceos = 0;
     int procesosListos = 5;
 
     private static void llenarProcesos(int nProcesos){
         // esto esta más facil ahora, solo hay que llenar la cola de procesos y los primeros 5 son en nuevo
+        //esto les va a poner "Nuevo" a todos los procesos
         for (int i = 0; i < nProcesos; i++) {
-            colaProcesos.add(new Procesos());
+            Procesos p = new Procesos();
+            colaProcesos.add(p);
+            p.setEstado("Nuevo");
         }
     }
+
 
     private static void etiquetarProcesos(){
         // etiquetar los primeros 5 con tiempo de llegada de 0
@@ -34,6 +53,49 @@ public class Programa_03 extends AbstractProcesos implements SoInterface{
         * Algunos procesos tendran la etiqueta de bloqueado ( I/O por el usuario ) y se desbloquearan en 8 ticks de tiempo
         * Algunos procesos tendran la etiqueta de terminado
         * */
+        // Esto le pone "Listo" solo al numero de procesosPrioritarios; = 5
+        int procesosPrioritarios = 5;
+        for (int i = 0; i < procesosPrioritarios; i++) {
+            if (i < colaProcesos.size()){
+                Procesos p = colaProcesos.get(i);
+                p.setEstado("Listo");
+                colaProcesosPrioritario.addLast(p);
+            }
+        }
+    }
+
+    private static void ejecutarProcesos(){
+
+        while (!colaProcesosPrioritario.isEmpty()){
+            // actualizar el tiempo gloobal
+            //
+            tiempoTranscurridoTicks+=ticksTiempoMilis;
+            clearScreen();
+            imprimirProcesos();
+            try{Thread.sleep(ticksTiempoMilis);}catch (InterruptedException ex){
+                System.out.println("Ocurrio un problema en el hilo");}
+
+            Procesos p = colaProcesosPrioritario.getFirst();
+            p.setEstado("Ejecucion");
+            p.setTiempoMax(p.getTiempoMax()-1);
+
+            if (p.getTiempoMax() <= 0){
+                p.setEstado("Terminado");
+                colaProcesosPrioritario.removeFirst();
+
+                //inentar guardar el indice para acceder a la colaProcesos por el indice
+                for (Procesos pp: colaProcesos){
+                    if (pp.getEstado().contains("Nuevo")){
+                        colaProcesosPrioritario.add(pp);
+                        // break del segundo bucle a la primera coincidencia con "Nuevo"
+                        break;
+                    }
+                }
+            }
+
+        }
+
+
     }
 
 
@@ -57,14 +119,27 @@ public class Programa_03 extends AbstractProcesos implements SoInterface{
         teclado.nextLine();
         llenarProcesos(nProcesos);
         etiquetarProcesos();
+        imprimirProcesos();
+        ejecutarProcesos();
 
     }
 
+    private static void imprimirProcesos(){
+        for (Procesos p: colaProcesos){
+            System.out.printf("\nPID: %d | TME: %d | Operacion: %s | Valor A: %d, Valor B: %d | Estado: %s"
+                    ,p.getPID(), p.getTiempoMax(), p.getOperacion(), p.getOperadores()[0], p.getOperadores()[1], p.getEstado());
+        }
+    }
 
-
+    static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
     int tiempoDelay = 500;
     private static void modoTest(){
         llenarProcesos(24);
     }
+
+
 }
